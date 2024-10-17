a82bbb26704c0dc5c55dec36b81ce6633267b3b9
HenryOlvera28 <henrymoises2815@gmail.com>
2024-10-16 22:28:35 -0500

    Create UserRepository.java

---
+++ Taller_Solid_Base/Taller_Solid_Base/src/main/java/UserRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea1henryolvera;
+
+/**
+ *
+ * @author henry
+ */
+public class UserRepository {
+    private Database database;
+
+    public UserRepository(Database database) {
+        this.database = database;
+    }
+
+    public void insertUser(User user) {
+        //codigo para insertar al usuario en la Database
+    }
+}
