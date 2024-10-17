6f3b789bf52b849717c22850d66ccb7ec5fa6bdd
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-10-16 22:08:40 -0500

    Update LogIn.java
    Modificacion interfaz Login

--- Taller_Solid_Base/Taller_Solid_Base/src/main/java/LogIn.java
+++ Taller_Solid_Base/Taller_Solid_Base/src/main/java/LogIn.java
@@ -1,12 +1,5 @@
-public class LogIn {
-    public void log (User user) {
-        System.out.println("Has access to the website");
-        insertUserInDatabase(user);
-        // Logic
-    }
-    public void insertUserInDatabase(User user){
-        // Insert user in database
-    }
+public interface LogIn {
+    public void log (User user);
 }
 
 
